b242702a6db302ca8f2595852bee7509c95581bc
amelie-certin <amelie.certin@gmail.com>
2016-11-19 18:48:44 +0100

    add dictionary

--- src/dictionary.c
+++ src/dictionary.c
@@ -1,1 +1,23 @@
 #include "dictionary.h"
+
+
+#include <string.h>
+
+bool createDictionary(const char *filename) {
+  FILE *file = openDictionaryFile(filename, "w");
+  if(!file) {
+    return false;
+  }
+  fprintf(file, "=== DICTIONARY TYPE ===\n");
+  fclose(file);
+  return createMetadata(filename);
+}
+
+FILE* openDictionaryFile(const char *filename, const char *rights) {
+  char *filename_ext = malloc(strlen(filename) + 32);
+  sprintf(filename_ext, "resources/dictionaries/%s.dic", filename);
+  FILE *file = fopen(filename_ext, rights);
+  free(filename_ext);
+  filename_ext = NULL;
+  return file;
+}
