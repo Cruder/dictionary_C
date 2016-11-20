071197afd574c5ee8829dd38ba3eba31f72f2b0f
amelie-certin <amelie.certin@gmail.com>
2016-11-20 19:27:22 +0100

    select dictionary

--- src/dictionary.c
+++ src/dictionary.c
@@ -8,14 +8,22 @@
  * \brief Function for creating an empty dictionary file
  * \return Boolean of success or not
  */
-bool createDictionary(const char *filename) {
-  FILE *file = openDictionaryFile(filename, "w");
-  if(!file) {
-    return false;
-  }
-  fprintf(file, "=== DICTIONARY TYPE ===\n");
-  fclose(file);
-  return createMetadata(filename);
+ bool createDictionary(const char *filename) {
+   FILE *file = openDictionaryFile(filename, "w");
+   if(!file) {
+     return false;
+   }
+   fprintf(file, "=== DICTIONARY TYPE ===\n");
+   fclose(file);
+   return createMetadata(filename);
+ }
+
+Dictionary *emptyDictionary() {
+  Dictionary *d = malloc(sizeof(Dictionary));
+  d->metadata = NULL;
+  d->file = NULL;
+  d->filename = NULL;
+  return d;
 }
 
 /**
@@ -33,4 +41,27 @@ FILE* openDictionaryFile(const char *filename, const char *rights) {
   free(filename_ext);
   filename_ext = NULL;
   return file;
+}
+
+void freeDictionary(Dictionary *dico) {
+  freeMetadata(dico->metadata);
+  dico->metadata = NULL;
+  if(dico->file) {
+    fclose(dico->file);
+  }
+  dico->file = NULL;
+  free(dico->filename);
+  dico->filename = NULL;
+  free(dico);
+  dico = NULL;
+}
+
+Dictionary* selectDictionary(const char *filename) {
+  Dictionary *dico;
+  dico = emptyDictionary();
+  dico->filename = malloc(sizeof(char) * (strlen(filename) + 1));
+  strcpy(dico->filename, filename);
+  dico->metadata = loadMetadata(filename);
+  displayMetadata(dico->metadata);
+  return dico;
 }
--- src/dictionary.h
+++ src/dictionary.h
@@ -7,15 +7,25 @@
   #define clear() system("clear")
 #endif
 
-#include "metadata.h"
 #include "input.h"
+#include "metadata.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
+
+typedef struct {
+  Metadata *metadata;
+  FILE *file;
+  char *filename;
+} Dictionary;
+
+Dictionary *emptyDictionary();
 bool createDictionary(const char *filename);
 FILE* openDictionaryFile(const char *filename, const char *rights);
+void freeDictionary(Dictionary *dico);
+Dictionary* selectDictionary();
 
 #endif //__DICTIONARY_H__
