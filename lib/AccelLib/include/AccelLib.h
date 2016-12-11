bac363e49d8a90f9e0544099c1ef8aed62d90eaf
Tristan <TristanEduProjet@users.noreply.github.com>
2016-12-11 12:55:20 +0100

    Fix include style for Unix

--- lib/AccelLib/include/AccelLib.h
+++ lib/AccelLib/include/AccelLib.h
@@ -8,24 +8,24 @@ Fichier raccourci incluant toutes les sous-parties.
 */
 
 /** Permet de tester/détecter l'environnement de compilation */
-//#include "AccelLib\testOS.h"
+//#include <AccelLib/testOS.h>
 
 /** pseudo-librairie de logging légère */
-//#include "AccelLib\_log.h"
+//#include <AccelLib/_log.h>
 
 /** Fonctionnalités relative au temps */
-#include "AccelLib\time_.h"
+#include <AccelLib/time_.h>
 
 /** Permet une utilisation avancée du terminal */
-#include "AccelLib\terminal.h"
+#include <AccelLib/terminal.h>
 
 /** Gestion légère des versions */
-#include "AccelLib\version.h"
+#include <AccelLib/version.h>
 
 /** Fonctionnalités relative aux chaines de caractère */
-#include "AccelLib\strings.h"
+#include <AccelLib/strings.h>
 
 /** Fonctionnalités relative aux fichiers */
-#include "AccelLib\file.h"
+#include <AccelLib/file.h>
 
 #endif // ACCELLIB_H_INCLUDED
