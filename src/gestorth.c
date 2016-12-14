3faa32b9fb8925164f8db7a72ecba0f8b21c308d
alex lairan <lairana@free.fr>
2016-12-14 10:25:26 +0100

    Use linked list

--- src/gestorth.c
+++ src/gestorth.c
@@ -1,4 +1,5 @@
 #include "gestorth.h"
+#include "linked_words.h"
 
 /**
  * \param dictionary The dictionary who contains words
@@ -15,20 +16,42 @@ void listUnexistsWords(Dictionary *dictionary, const char *filename) {
 
     char *str = malloc(sizeof(char) * 255);
 
+    bool is_first_word = false;
+    LinkedWords *first_linked_word = NULL;
+    LinkedWords *next_linked_word = NULL;
+    size_t counter = 0;
+    size_t line = 0;
     while(!feof(file)) {
         fscanf(file, "%s", str);
+        int newline = fgetc(file);
+        if(newline == '\n') {
+            ++line;
+        }
+        if(!feof(file)) {
+            fseek(file, -1, SEEK_CUR);
+        }
         cleanWord(str);
         // strlen(str) > 0 because of punctuation removal
         size_t wordlen = strlen(str);
         if(wordlen > 0 && wordPresent(dictionary, str) == false) {
-            // Should be store into an array
-            printf("\t> %s\n", str);
-            // Clean word
+            printf("\t%zu > %s\n", line, str);
+            if(is_first_word == false) {
+                is_first_word = true;
+                first_linked_word = newWord(str);
+                next_linked_word = first_linked_word;
+            } else {
+                next_linked_word->next = newWord(str);
+                next_linked_word = next_linked_word->next;
+            }
+            ++counter;
             for(size_t i = 0; i < wordlen; ++i) {
                 str[i] = '\0';
             }
         }
     }
+
+    printf("Line %zu\n", line);
+    // displayLinkedWord(first_linked_word);
 
     free(str);
     fclose(file);
--- src/main3.c
+++ src/main3.c
@@ -1,6 +1,9 @@
 #include "gestorth.h"
 
 int main(void) {
-    printf("Hello Word\n");
+    // Should use the menu3 method
+    Dictionary *dico = selectDictionary("demo");
+    listUnexistsWords(dico, "yolo.txt");
+    freeDictionary(dico);
     return 0;
 }
